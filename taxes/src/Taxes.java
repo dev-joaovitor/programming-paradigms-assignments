c19f62062fc2a2d11deb853b95d83ba7ac30e2d2
João <116603666+dev-joaovitor@users.noreply.github.com>
2024-10-08 18:51:50 -0300

    :broom: minor changes on taxes project

--- taxes/src/Taxes.java
+++ taxes/src/Taxes.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-class Taxes {
+public class Taxes {
 	private double annualSalaryIncome;
 	private double annualServiceProvisioningIncome;
 	private double annualCapitalGain;
 	private double medicalSpending;
 	private double educationalSpending;
-	private static double SERVICE_PROVISIONING_TAX_RATE = (double) 15/100;
-	private static double CAPITAL_GAIN_TAX_RATE = (double) 20/100;
-	private static double MAX_DEDUCTIBLE_RATE = (double) 30/100;
+	private static double SERVICE_PROVISIONING_TAX_RATE = 15.0/100.0;
+	private static double CAPITAL_GAIN_TAX_RATE = 20.0/100.0;
+	private static double MAX_DEDUCTIBLE_RATE = 30.0/100.0;
 
 	public void setAnnualSalaryIncome(double income) {
 		this.annualSalaryIncome = income;
@@ -38,9 +38,9 @@ class Taxes {
 		double salaryTaxRate = 0;
 		
 		if (monthlySalaryIncome >= 3000 && monthlySalaryIncome < 5000) {
-			salaryTaxRate = (double) 10/100;
+			salaryTaxRate = 10.0/100.0;
 		} else if (monthlySalaryIncome >= 5000) {
-			salaryTaxRate = (double) 20/100;
+			salaryTaxRate = 20.0/100.0;
 		}
 		
 		System.out.println("### INCOME TAX REPORT ###");
@@ -48,17 +48,17 @@ class Taxes {
 		System.out.println("* INCOME ESTABLISHED");
 		
 		double salaryIncomeTax = annualSalaryIncome * salaryTaxRate;
-		System.out.println("Salary income tax: " + salaryIncomeTax);
+		System.out.printf("Salary income tax: R$%.2f", salaryIncomeTax);
 		
 		double serviceProvisioningIncomeTax =
 			annualServiceProvisioningIncome * SERVICE_PROVISIONING_TAX_RATE;
-		System.out.println(
-			"Service provisioning income tax: "
-			+ serviceProvisioningIncomeTax
+		System.out.printf(
+			"Service provisioning income tax: R$%.2f"
+			, serviceProvisioningIncomeTax
 		);
 		
 		double capitalGainTax = annualCapitalGain * CAPITAL_GAIN_TAX_RATE;
-		System.out.println("Capital gain tax: " + capitalGainTax);
+		System.out.printf("Capital gain tax: R$%.2f", capitalGainTax);
 		System.out.println();
 		System.out.println("* DEDUCTIONS");
 		
@@ -66,7 +66,7 @@ class Taxes {
 			salaryIncomeTax + serviceProvisioningIncomeTax + capitalGainTax;
 		
 		double maxDeductible = dueTaxes * MAX_DEDUCTIBLE_RATE;
-		System.out.println("Maximum deductible: " + maxDeductible);
+		System.out.printf("Maximum deductible: R$%.2f", maxDeductible);
 		
 		double deductibleExpenses = educationalSpending + medicalSpending;
 		double deduction = deductibleExpenses;
@@ -75,15 +75,15 @@ class Taxes {
 			deduction = maxDeductible;
 		}
 	
-		System.out.println("Deductible expenses: " + deductibleExpenses);
+		System.out.printf("Deductible expenses: R$%.2f", deductibleExpenses);
 		System.out.println();
 		
 		System.out.println("* SUMMARY");
-		System.out.println("Gross taxes: " + dueTaxes);
-		System.out.println("Deduction: " + deduction);
+		System.out.printf("Gross taxes: R$%.2f", dueTaxes);
+		System.out.printf("Deduction: R$%.2f", deduction);
 		
 		dueTaxes -= deduction;
-		System.out.println("Due taxes: " + dueTaxes);
+		System.out.printf("Due taxes: R$%.2f", dueTaxes);
 		System.out.println("-----------------------------");
 	}
 
